840782bbbb2e12356c070e785ba1b138171dfbb6
Philip Bergman <philip@zicht.nl>
2015-11-24 16:46:56 +0000

    add some commands
    git-svn-id: svn://zon/projecten/zicht-libraries/z-plugin-content/trunk@74058 0f35748b-8228-47f0-bbea-889afbbd9a42

--- content/Plugin.php
+++ content/Plugin.php
@@ -8,8 +8,9 @@
 
 namespace Zicht\Tool\Plugin\Content;
 
-use \Zicht\Tool\Plugin as BasePlugin;
-use \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Zicht\Tool\Container\Container;
+use Zicht\Tool\Plugin as BasePlugin;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
 /**
  * Content plugin
@@ -32,6 +33,10 @@ class Plugin extends BasePlugin
                             ->prototype('scalar')->end()
                             ->performNoDeepMerging()
                         ->end()
+                        ->arrayNode('exclude')
+                            ->prototype('scalar')->end()
+                            ->performNoDeepMerging()
+                        ->end()
                         ->arrayNode('db')
                             ->children()
                                 ->arrayNode('structure')
@@ -50,5 +55,56 @@ class Plugin extends BasePlugin
                 ->end()
             ->end()
         ;
+    }
+
+
+    /**
+     * @{inheritDoc}
+     */
+    public function setContainer(Container $container)
+    {
+        $container->decl(['content','exclude_string'], function(Container $c) {
+
+            if (empty($c->resolve('content.exclude'))) {
+                return null;
+            }
+            $return = [];
+            foreach ($c->resolve('content.exclude') as $exlude) {
+                $return[] = sprintf('--exclude=%s', $exlude);
+            }
+            return implode(' ', $return);
+
+        });
+        $container->decl(['content','push_backup_file'], function(Container $c) {
+            return sprintf(
+                '%s.%s.%s%s%s.tar.gz',
+                (new \DateTime())->format('Ymd.U'),
+                $c->resolve('target_env'),
+                $c->resolve('envs')[$c->resolve('target_env')]['db'],
+                (!empty($c->resolve('table'))) ? '.' . $c->resolve('table') : null,
+                (!empty($c->resolve('where'))) ? '.' .rtrim(base64_encode($c->resolve('where')),'=') : null
+            );
+        });
+        $container->decl(['content','sql_where'], function(Container $c) {
+            return (!empty($c->resolve('where'))) ? sprintf('--where=\'%s\' ',$c->resolve('where')) : null;
+        });
+
+        $container->decl(['content','local_db_args'], function(Container $c) {
+            $line[] = sprintf('-u%s', !empty($c->resolve('local_user')) ? $c->resolve('local_user') : 'dev');
+            $line[] = sprintf('-h%s', !empty($c->resolve('local_host')) ? $c->resolve('local_host') : 'dev3');
+            if (!empty($c->resolve('local_password'))) {
+                $line[] = sprintf('-p%s', $c->resolve('local_password'));
+            }
+            if (!empty($c->resolve('local_port'))) {
+                $line[] = sprintf('-P%s', $c->resolve('local_port'));
+            }
+            if (false === ($local = $c->resolve('local_db'))) {
+                $line[] = $c->resolve('envs')[$c->resolve('target_env')]['db'] . '.local';
+            } else {
+                $line[] = $local;
+            }
+            return implode(' ', $line);
+        });
+
     }
 }
