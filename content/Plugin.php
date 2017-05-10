3a9838ecc97615209a9f8f9e0669dc60a39bd424
philip <philip@zicht.nl>
2017-05-10 14:19:04 +0200

    new logic for db push/pull

--- content/Plugin.php
+++ content/Plugin.php
@@ -8,6 +8,7 @@
 
 namespace Zicht\Tool\Plugin\Content;
 
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Zicht\Tool\Container\Container;
 use Zicht\Tool\Plugin as BasePlugin;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
@@ -52,7 +53,6 @@ class Plugin extends BasePlugin
         ;
     }
 
-
     /**
      * @{inheritDoc}
      */
@@ -74,47 +74,161 @@ class Plugin extends BasePlugin
         );
 
         $container->decl(
-            ['fmt', 'mysql_pull'],
+            ['fmt', 'cmd', 'mysql_local_drop'],
             function(Container $c) {
-                $line = [];
                 if ($c->resolve('drop') && !$c->resolve('table')) {
-                    $line[] = ["mysql", 0];
-                    $line[] = [$c->resolve('fmt.local_mysql_args'), 8];
-                    $line[] = [sprintf('-e "DROP DATABASE IF EXISTS %1$s; CREATE DATABASE %1$s;" &&', $c->resolve('content.local_db')), 8];
-                }
-                $line[] = [sprintf('ssh %s', $c->resolve(sprintf('envs.%s.ssh', $c->resolve('target_env')))), 4];
-                $line[] = ['"mysqldump', 8];
-                $line[] = ['--quote-names', 12];
-                $line[] = ['--opt', 12];
-                if ($c->resolve('VERBOSE')) {
-                    $line[] = ['--verbose', 12];
-                }
-                if ("" !== ($where = $c->resolve('where'))) {
-                    $line[] = [sprintf('--where="%s"', $where), 12];
-                }
-                $line[] = [sprintf('%s', $c->resolve(sprintf('envs.%s.db', $c->resolve('target_env')))), 12];
-                if (null !== ($table = $c->resolve('table'))) {
-                    $line[] = [$table, 12];
-                }
-                $line[] = ['| gzip -c -9 "', 12];
-                if ("" !== ($file = $c->resolve('file'))) {
-                    $line[] = [sprintf('> %s', $file), 4];
+                    return sprintf(
+                        'mysql %1$s -e "DROP DATABASE IF EXISTS %2$s; CREATE DATABASE %2$s;',
+                        $c->resolve('fmt.local_mysql_args'),
+                        $c->resolve('content.local_db')
+                    );
                 } else {
-                    $line[] = [sprintf('| gzip -d -', $file), 4];
-                    $line[] = [sprintf('| mysql %s %s',  $c->resolve('fmt.local_mysql_args'), $c->resolve('content.local_db')), 4];
+                    return null;
                 }
-                return implode(
-                    " \\\n",
-                    array_filter(
-                        array_map(
-                            function($v) {
-                                list($value, $indent) = $v;
-                                return !(empty($value)) ? str_repeat(" ", $indent) . $value : null;
-                            },
-                            $line
-                        )
-                    )
+            }
+        );
+
+        $container->decl(
+            ['fmt', 'cmd', 'mysql_pull'],
+            function(Container $c) {
+                $mysqldump  =  "mysqldump --opt";
+                $mysqldump .=  ($c->resolve('VERBOSE')) ? ' -Qv' : " -Q";
+                if ($where = $c->resolve('where')) {
+                    $mysqldump .= sprintf(' --where="%s"', $where);
+                }
+                $mysqldump .= " " . $c->resolve(sprintf('envs.%s.db', $c->resolve('target_env')));
+                if ($table = $c->resolve('table')) {
+                    $mysqldump .= " ${table}";
+                }
+                if ($file = $c->resolve('file')) {
+                    $out = ($file === '-') ? "> /dev/stdout" : "> ${file}";
+                } else {
+                    $out = sprintf(
+                        "| gzip -d - | mysql %s %s",
+                        $c->resolve('fmt.local_mysql_args'),
+                        $c->resolve('content.local_db')
+                    );
+                }
+                $ssh = $c->resolve(sprintf('envs.%s.ssh', $c->resolve('target_env')));
+                return  "ssh ${ssh} \"${mysqldump}\" | gzip -c -9 ${out}";
+            }
+        );
+
+
+        $container->decl(
+            ['fmt', 'cmd', 'mysql_remote_backup'],
+            function(Container $c) {
+                if ($c->resolve('backup')) {
+                    $cmd = "mysqldump --opt ";
+                    if ($c->resolve('VERBOSE')) {
+                        $cmd .= "-Qv ";
+                    } else {
+                        $cmd .= "-Q ";
+                    }
+                    if ($where = $c->resolve('where')) {
+                        $cmd .= sprintf('--where="%s" ', $where);
+                    }
+                    $cmd .= $c->resolve(sprintf('envs.%s.db', $c->resolve('target_env'))) . " ";
+                    if ($table = $c->resolve('table')) {
+                        $cmd .= "${table} ";
+                    }
+                    $cmd .= sprintf(" | gzip -c -9 > %s", $c->resolve('fmt.sql_backup_file'));
+                    return $cmd;
+                } else {
+                    return null;
+                }
+            }
+        );
+
+        $container->decl(
+            ['fmt', 'cmd', 'mysql_push'],
+            function(Container $c) {
+                if ($c->resolve('from_dump')) {
+                    $c->set('local_db', $c->resolve('arg'));
+                    $cmd = sprintf(
+                        "mysqldump --opt %s ",
+                        $c->resolve('fmt.local_mysql_args')
+                    );
+                    if ($c->resolve('VERBOSE')) {
+                        $cmd .= "-Qv ";
+                    } else {
+                        $cmd .= "-Q ";
+                    }
+                    if ($where = $c->resolve('where')) {
+                        $cmd .= sprintf('--where="%s" ', $where);
+                    }
+                    $cmd .= $c->resolve('content.local_db') . " ";
+                    if ($table = $c->resolve('table')) {
+                        $cmd .= "${table} ";
+                    }
+                    $cmd .= "| ";
+                } else {
+                    if ($file = $c->resolve('arg')) {
+                        $cmd = sprintf("cat %s | gzip -d - | ", $c->resolve('arg'));
+                    } else {
+                        throw new InvalidArgumentException("Missing required file arg.");
+                    }
+                }
+
+                $cmd .= sprintf(
+                    'ssh %s "mysql %s"',
+                    $c->resolve(sprintf('envs.%s.ssh', $c->resolve('target_env'))),
+                    $c->resolve(sprintf('envs.%s.db', $c->resolve('target_env')))
                 );
+                return $cmd;
+            }
+        );
+
+        $container->method(
+            ['fmt', 'cmd', 'rsync'],
+            function(Container $c, $src, $dest) {
+                if (null !== ($args = $c->resolve('rsync-flags'))) {
+                    return sprintf("rsync %s %s %s", $args, $src, $dest);
+                } else {
+                    $long = [];
+                    $short = [
+                        'r', //recursive
+                        'u', //update
+                    ];
+                    if ($c->resolve('VERBOSE')) {
+                        $short[] = "v"; // verbose
+                        $short[] = "h"; // human-readable;
+                        $short[] = "p"; // progress
+                    }
+                    if ($c->resolve('simulate')) {
+                        $short[] = "n"; // dry-run
+                    }
+                    if ($c->resolve('delete')) {
+                        $long[] = "delete";
+                    }
+                    if (null !== ($exclude = $c->resolve('content.exclude'))) {
+                        foreach((array) $exclude as $e) {
+                            $long[] = sprintf("exclude=%s", $e);
+                        }
+                    }
+                    return preg_replace(
+                        '#\s{2,}#',
+                        '',
+                        sprintf(
+                            "rsync -%s %s %s %s",
+                            implode(
+                                '',
+                                $short
+                            ),
+                            implode(
+                                " ",
+                                array_map(
+                                    function ($v) {
+                                        return  "--${v}";
+                                    },
+                                    $long
+                                )
+                            ),
+                            $src,
+                            $dest
+                        )
+                    );
+                }
             }
         );
 
@@ -176,7 +290,7 @@ class Plugin extends BasePlugin
                     return $file;
                 } else {
                     return sprintf(
-                        '%s.%s.%s%s%s.tar.gz',
+                        '%s.%s.%s%s%s.tgz',
                         (new \DateTime())->format('Ymd.U'),
                         $c->resolve('target_env'),
                         $c->resolve(sprintf('envs.%s.db', $c->resolve('target_env'))),
@@ -184,60 +298,7 @@ class Plugin extends BasePlugin
                         (!empty($c->resolve('where'))) ? '.' . rtrim(base64_encode($c->resolve('where')),'=') : null
                     );
                 }
-        });
-
-        $container->method(
-            ['fmt', 'cmd', 'rsync'],
-            function(Container $c, $src, $dest) {
-                if (null !== ($args = $c->resolve('rsync-flags'))) {
-                    return sprintf("rsync %s %s %s", $args, $src, $dest);
-                } else {
-                    $long = [];
-                    $short = [
-                        'r', //recursive
-                        'u', //update
-                    ];
-                    if ($c->resolve('VERBOSE')) {
-                        $short[] = "v"; // verbose
-                        $short[] = "h"; // human-readable;
-                        $short[] = "p"; // progress
-                    }
-                    if ($c->resolve('simulate')) {
-                        $short[] = "n"; // dry-run
-                    }
-                    if ($c->resolve('delete')) {
-                        $long[] = "delete";
-                    }
-                    if (null !== ($exclude = $c->resolve('content.exclude'))) {
-                        foreach((array) $exclude as $e) {
-                            $long[] = sprintf("exclude=%s", $e);
-                        }
-                    }
-                    return preg_replace(
-                        '#\s{2,}#',
-                        '',
-                        sprintf(
-                            "rsync -%s %s %s %s",
-                            implode(
-                                '',
-                                $short
-                            ),
-                            implode(
-                                " ",
-                                array_map(
-                                    function ($v) {
-                                        return  "--${v}";
-                                    },
-                                    $long
-                                )
-                            ),
-                            $src,
-                            $dest
-                        )
-                    );
-                }
-            }
-        );
+            });
 
         $container->decl(
             ['content', 'local_db'],
